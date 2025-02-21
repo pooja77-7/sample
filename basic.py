f9ff45e89a894cef22c31641b72850590690c4a9
pooja77-7 <konipooja7@gmail.com>
2025-02-21 11:09:20 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,32 @@
+#printing messages 
+print("welcom")
+
+#variable  types 
+#1.camel case
+schoolName="aditya"  #use n java
+
+#2.snake case
+school_name="sasi"  #use in python
+
+#3.pascal case
+SchoolName="mahati"  #use in c#
+
+
+#input
+# input is used to take user input,& print is display to output
+name=input("what is your name ")
+print(name)
+print()
+
+int_1=int(input("enter the number1 "))
+int_2=int(input("enter the number2 "))
+result=int_1+int_2
+print(result)
+print(f"number value{int_1} and number2 vaule {int_2}performed addition opertion and result is {result}")
+print()
+
+user_name="pooja"
+last_name="koni"
+print(user_name+" "+last_name)
+print()
+
